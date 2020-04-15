b87d6c9e62078089f1eb38024ebe7aa0c765a99f
maximfreerider <ivanovma41@gmail.com>
2020-04-15 22:57:38 +0300

    fixed bug in UserSerializer, added create method

--- django_rest_user_service/user_service/serializers.py
+++ django_rest_user_service/user_service/serializers.py
@@ -12,8 +12,14 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-    group = GroupSerializer()
-	# group = serializers.SerializerMethodField()
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'password', 'email', 'group', 'is_superuser')
+	group = GroupSerializer()
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'first_name', 'last_name', 'password', 'email', 'group', 'is_superuser')
+
+	def create(self, validated_data):
+		group_validated_data = validated_data.pop('group')
+		user = User.objects.create(**validated_data)
+		group_serializer = self.fields['group']
+		groups = group_serializer.create(group_validated_data)
+		return user
